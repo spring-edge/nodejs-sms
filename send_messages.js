a5514f7bc996f25c8e7af51311f10996fe37cb82
balwant.singh <balwant.singh@impelsys.com>
2017-07-12 21:04:40 +0530

    error fix

--- send_messages.js
+++ send_messages.js
@@ -3,15 +3,15 @@
 var springedge = require('springedge');
 
 var params = {
-  'apikey': '636n033l3549o14yp1ljdti3t81rk11v5', //TEST API Key
+  'apikey': 'xxxxxxxxxxxxxxxxx', //API Key
   'sender': 'SEDEMO', //Test Sender
   'to': [
-    '919035732236'  //Test Numberss
+    '9190xxxxxxxx'  //Mobile Numberss
   ],
-  'body': 'test+message'
+  'message': 'test message from node js'
 };
 
-springedge.messages.send(params, function (err, response) {
+springedge.messages.send(params, 5000, function (err, response) {
   if (err) {
     return console.log(err);
   }
